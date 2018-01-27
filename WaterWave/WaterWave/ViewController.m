9679d0116288f0ea3f4d4bcb6013f10ab8db41c5
ouyangxiongchun <ouyxc1145@2ncai.com>
2018-01-27 17:37:28 +0800

    qqqq

--- WaterWave/WaterWave/ViewController.m
+++ WaterWave/WaterWave/ViewController.m
@@ -20,6 +20,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.view.backgroundColor = [UIColor whiteColor];
     _loadingView = [YDWaveLoadingView loadingView];
     [self.view addSubview:_loadingView];
